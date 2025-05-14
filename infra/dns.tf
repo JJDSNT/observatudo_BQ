bb0de9ac7a12421e5e13d3cdba60012d83fa3971
Jaime Dias <jaimejosediasnt@gmail.com>
2025-05-14 03:52:58 -0300

    Fix: root domain redirection

--- infra/dns.tf
+++ infra/dns.tf
@@ -9,13 +9,3 @@ resource "google_dns_record_set" "www" {
   ]
 }
 
-# observatudo.com.br → mesmo serviço, mas via redirecionamento interno
-resource "google_dns_record_set" "root" {
-  name         = "observatudo.com.br."
-  type         = "CNAME"
-  ttl          = 300
-  managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
-  rrdatas = [
-    "${replace(google_cloud_run_service.www_observatudo.status[0].url, "https://", "")}."
-  ]
-}
--- infra/domain.tf
+++ infra/domain.tf
@@ -10,3 +10,16 @@ resource "google_cloud_run_domain_mapping" "www" {
     route_name = google_cloud_run_service.www_observatudo.name
   }
 }
+
+resource "google_cloud_run_domain_mapping" "root" {
+  name     = "observatudo.com.br"
+  location = var.region
+
+  metadata {
+    namespace = var.project_id
+  }
+
+  spec {
+    route_name = google_cloud_run_service.www_observatudo.name
+  }
+}
