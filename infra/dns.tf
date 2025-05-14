c155b3b3b71f89623d71c2b510efaebb8c5e79e3
Jaime Dias <jaimejosediasnt@gmail.com>
2025-05-14 01:49:24 -0300

    Fix: bad syntax

--- infra/dns.tf
+++ infra/dns.tf
@@ -4,7 +4,9 @@ resource "google_dns_record_set" "www" {
   type         = "CNAME"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
-  rrdatas      = [google_cloud_run_service.www_observatudo.status[0].url]
+  rrdatas = [
+    "${replace(google_cloud_run_service.www_observatudo.status[0].url, "https://", "")}."
+  ]
 }
 
 # observatudo.com.br → mesmo serviço, mas via redirecionamento interno
@@ -13,5 +15,7 @@ resource "google_dns_record_set" "root" {
   type         = "CNAME"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
-  rrdatas      = [google_cloud_run_service.www_observatudo.status[0].url]
+  rrdatas = [
+    "${replace(google_cloud_run_service.www_observatudo.status[0].url, "https://", "")}."
+  ]
 }
