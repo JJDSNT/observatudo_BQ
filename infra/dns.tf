33b4326422d0da83854c9ac5db2888247280c64d
Jaime Dias <jaimejosediasnt@gmail.com>
2025-05-14 00:28:17 -0300

    infra code

--- infra/dns.tf
+++ infra/dns.tf
@@ -3,7 +3,7 @@ resource "google_dns_record_set" "www" {
   name         = "www.observatudo.com.br."
   type         = "CNAME"
   ttl          = 300
-  managed_zone = google_dns_managed_zone.observatudo_com_br.name
+  managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
   rrdatas      = [google_cloud_run_service.www_observatudo.status[0].url]
 }
 
@@ -12,6 +12,6 @@ resource "google_dns_record_set" "root" {
   name         = "observatudo.com.br."
   type         = "CNAME"
   ttl          = 300
-  managed_zone = google_dns_managed_zone.observatudo_com_br.name
+  managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
   rrdatas      = [google_cloud_run_service.www_observatudo.status[0].url]
 }
---
+++ infra/zone_data.tf
@@ -0,0 +1,3 @@
+data "google_dns_managed_zone" "observatudo_com_br" {
+  name = "observatudo-zone"
+}
