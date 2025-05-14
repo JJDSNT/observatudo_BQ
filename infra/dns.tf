bcd858cb8a0e4ae608ec4e3a0482b01d9935873d
Jaime Dias <jaimejosediasnt@gmail.com>
2025-05-14 13:59:05 -0300

    FIX: cname setup

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,10 +1,7 @@
 resource "google_dns_record_set" "www" {
-  name         = "www.observatudo.com.br."
-  type         = "CNAME"
-  ttl          = 300
-  managed_zone = data.google_dns_managed_zone.observatudo_com_br.name
-  rrdatas = [
-    "${replace(google_cloud_run_service.www_observatudo.status[0].url, "https://", "")}."
-  ]
+  name          = "www.observatudo.com.br."
+  type          = "CNAME"
+  ttl           = 300
+  managed_zone  = data.google_dns_managed_zone.observatudo_com_br.name
+  rrdatas       = ["ghs.googlehosted.com."]
 }
-
