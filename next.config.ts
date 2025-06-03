9274bedf2c02ec5843c2a393a13d17cfb8869f4f
Jaime Dias <jaimejosediasnt@gmail.com>
2025-06-03 15:34:47 -0300

    Add pwa support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+
+const withPWAConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
 
 const nextConfig: NextConfig = {
-
   async redirects() {
     return [
       {
@@ -12,7 +31,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  
 };
 
-export default nextConfig;
+export default withPWAConfig(nextConfig);
