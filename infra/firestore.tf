83d371dce2b1312133e153903a5e1913bc9a9aeb
Jaime Dias <jaimejosediasnt@gmail.com>
2025-06-04 07:43:49 -0300

    Update firebase setup

--- infra/firestore.tf
+++ infra/firestore.tf
@@ -13,4 +13,8 @@ resource "google_firestore_database" "default" {
   name        = "(default)"
   location_id = var.firestore_location
   type        = "FIRESTORE_NATIVE"
+  
+  depends_on = [
+    google_project_service.firestore
+  ]
 }
