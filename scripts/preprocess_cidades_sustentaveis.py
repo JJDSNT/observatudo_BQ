311dc98522271e9231d395f43a4e75f550ddec0d
Jaime Dias <jaimejosediasnt@gmail.com>
2025-06-16 15:36:41 -0300

    ADD pth resolution

--- scripts/preprocess_cidades_sustentaveis.py
+++ scripts/preprocess_cidades_sustentaveis.py
@@ -1,5 +1,11 @@
 # scripts/preprocess_cidades_sustentaveis.py
-from observatudo.transformers.cidades_sustentaveis import main
 
 if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+    from observatudo.transformers.cidades_sustentaveis import main
+
     main()
